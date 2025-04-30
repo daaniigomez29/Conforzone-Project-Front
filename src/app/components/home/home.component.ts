49d9cf645caf2f2b4646dcf2f74b8702d8edf710
Daniel Gómez <danigb2904@gmail.com>
2025-04-30 19:08:37 +0200

    requestBudget 50%

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { NavigationStart, Router, RouterModule } from '@angular/router';
 import { INSTALLATIONS } from '../../js/installations';
 import { ModalService } from '../../services/modal.service';
+import { SpecificServiceService } from '../../services/specific-service.service';
+import { SpecificService } from '../../interfaces/SpecificService';
 
 @Component({
   selector: 'app-home',
@@ -24,18 +26,39 @@ export class HomeComponent implements OnInit, OnDestroy{
 
   //Step 2 (Services)
   installationsType = INSTALLATIONS;
-  installationService = '';
+  installationServiceSlug = '';
 
   //Step 2 (Offers)
 
-  //Step 3 ()
+  //Step 3 (Services)
+  specificServicesArray:SpecificService[] = []
+  specificServiceChoosed: SpecificService = {
+    id: -1,
+    name: "",
+    description: "",
+    slug: "",
+    firstPrice: 0,
+    pricePerMeter: 0,
+    available: false,
+    offer: false
+  }
+
+  //Step 4 (Services)
+  quantity: number = 1
+  totalPrice: number = 0
+
+
+  //Step 5 (Services)
+  totalPriceWithAdditionalMeter = 0
+
+  quantityAdditionalMeter: number = 0
 
   enviarPresupuesto() {
     this.step = 1
     // Aquí puedes cerrar el modal manualmente si usas ViewChild o servicios
   }
 
-  constructor(private router: Router, private modalService:ModalService) { }
+  constructor(private router: Router, private modalService:ModalService, private specificServiceService:SpecificServiceService) { }
 
   ngOnInit() {
     this.routerSubscription = this.router.events.subscribe(event => {
@@ -48,6 +71,61 @@ export class HomeComponent implements OnInit, OnDestroy{
   ngOnDestroy() {
     if (this.routerSubscription && !this.routerSubscription.unsubscribe()) {
       this.routerSubscription.unsubscribe()
+    }
+  }
+
+  getAllSpecificServicesBySlug() {
+    this.specificServicesArray = []
+    this.step = 3
+    this.specificServiceService.getAllSpecificServicesBySlug(this.installationServiceSlug).subscribe({
+      next: data => {
+        this.specificServicesArray = data
+      }
+    })
+  }
+
+  getSpecificServiceChoosed() {
+    this.step = 4
+    this.quantity = 1
+    this.totalPrice = 0
+    this.specificServiceService.getSpecificServiceBySlugAndId(this.installationServiceSlug, this.specificServiceChoosed.id).subscribe({
+      next: data => {
+        this.specificServiceChoosed.name = data.name;
+        this.specificServiceChoosed.firstPrice = data.firstPrice;
+        this.specificServiceChoosed.pricePerMeter = data.pricePerMeter;
+        
+        this.totalPrice = data.firstPrice
+      }
+    })
+  }
+
+  increment() {
+    this.quantity++;
+    this.totalPrice += this.specificServiceChoosed.firstPrice
+  }
+
+  decrement() {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.totalPrice -= this.specificServiceChoosed.firstPrice
+    }
+  }
+
+  goStep5(){
+    this.step = 5
+    this.quantityAdditionalMeter = 0
+    this.totalPriceWithAdditionalMeter = this.totalPrice
+  }
+
+  incrementAdditionalMeter() {
+    this.quantityAdditionalMeter++;
+    this.totalPriceWithAdditionalMeter += this.specificServiceChoosed.pricePerMeter
+  }
+
+  decrementAdditionalMeter() {
+    if (this.quantityAdditionalMeter > 0) {
+      this.quantityAdditionalMeter--;
+      this.totalPriceWithAdditionalMeter -= this.specificServiceChoosed.pricePerMeter
     }
   }
 }
